beaa13e040bf8a063b200dd906e0a7cf3e2ac91e
Sadanand Dodawadakar <sadanand.dodawadakar@mavenir.com>
2024-01-29 11:46:59 +0530

    Committing .md files

--- Intresting/try-catch-finalize/try.go
+++ Intresting/try-catch-finalize/try.go
@@ -2,18 +2,42 @@ package main
 
 import "fmt"
 
+type Exception interface{}
+
 type Block struct {
-	Try      func()
-	Catch    func(any)
-	Finalize func()
+	Try     func()
+	Catch   func(Exception)
+	Finally func()
 }
 
 func (b Block) Do() {
-	if b.Finalize != nil {
-		defer b.Finalize()
+	if b.Finally != nil {
+		defer b.Finally()
+	}
+	if b.Catch != nil {
+		defer func() {
+			r := recover()
+			if r != nil {
+				b.Catch(r)
+			}
+		}()
+	}
+	if b.Try != nil {
+		b.Try()
 	}
 }
 
 func main() {
 	fmt.Println("Demo Try Catch Finalize in Go...")
+	Block{
+		Try: func() {
+			fmt.Println("We are in Try block")
+		},
+		Catch: func(e Exception) {
+			fmt.Println("Received exception e")
+		},
+		Finally: func() {
+			fmt.Println("We have are in finally")
+		},
+	}.Do()
 }
